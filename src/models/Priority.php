8792ac966ceed664c57b45ddffcba51e75df86fe
Ahmed Kordy <ahmed@kordy.info>
2015-07-28 21:26:49 +0200

    Priority Model

---
+++ src/models/Priority.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kordy\Ticketit\Models;
+
+use Auth;
+use Illuminate\Database\Eloquent\Model;
+
+class Priority extends Model
+{
+    protected $table = 'ticketit_priorities';
+
+    protected $fillable = ['name', 'color'];
+
+    /**
+     * Indicates that this model should not be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Get related tickets
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tickets()
+    {
+        return $this->hasMany('Kordy\Ticketit\Models\Ticket', 'priority_id');
+    }
+}

b93066e1b5f2d94d84a40c66915b3c955ad91f27
Ahmed Kordy <ahmed@kordy.info>
2015-07-28 19:02:01 +0200

    added routes config lines

--- src/config/ticketit.php
+++ src/config/ticketit.php
@@ -6,5 +6,17 @@ return [
     * Template adherence: The master blade template to be extended
     * Default: resources/views/master.blade.php
     */
-    'master_template' => 'master'
+    'master_template' => 'master',
+
+    /*
+    * The route to tickets system (ex. http://url/tickets)
+    * Default: tickets
+    */
+    'main_route' => 'tickets',
+
+    /*
+    * The route to tickets admin dashboard (ex. http://url/tickets-admin)
+    * Default: tickets
+    */
+    'admin_route' => 'tickets-admin'
 ];

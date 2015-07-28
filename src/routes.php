68fb66fdbeff614d403f2f96d1ce20e8256e7a9e
Ahmed Kordy <ahmed@kordy.info>
2015-07-28 22:30:41 +0200

    Added Priority routes

--- src/routes.php
+++ src/routes.php
@@ -4,3 +4,6 @@ Route::resource(config('ticketit.main_route'), 'Kordy\Ticketit\Controllers\Ticke
 
 //Ticket admin routes (ex. http://url/tickets-admin/status)
 Route::resource(config('ticketit.admin_route').'/status', 'Kordy\Ticketit\Controllers\StatusesController');
+
+//Ticket admin routes (ex. http://url/tickets-admin/priority)
+Route::resource(config('ticketit.admin_route').'/priority', 'Kordy\Ticketit\Controllers\PrioritiesController');

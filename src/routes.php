cc41fabdd5f46effffcf383edb6c8a754eb529eb
Ahmed Kordy <ahmed@kordy.info>
2015-07-29 14:44:07 +0200

    Added route to the admin dashboard index page

--- src/routes.php
+++ src/routes.php
@@ -2,8 +2,11 @@
 //Ticket public route
 Route::resource(config('ticketit.main_route'), 'Kordy\Ticketit\Controllers\TicketsController');
 
-//Ticket admin routes (ex. http://url/tickets-admin/status)
+//Ticket admin index route (ex. http://url/tickets-admin/)
+Route::get(config('ticketit.admin_route'), 'Kordy\Ticketit\Controllers\PagesController@index');
+
+//Ticket statuses admin routes (ex. http://url/tickets-admin/status)
 Route::resource(config('ticketit.admin_route').'/status', 'Kordy\Ticketit\Controllers\StatusesController');
 
-//Ticket admin routes (ex. http://url/tickets-admin/priority)
+//Ticket priorities admin routes (ex. http://url/tickets-admin/priority)
 Route::resource(config('ticketit.admin_route').'/priority', 'Kordy\Ticketit\Controllers\PrioritiesController');

a34b49a2297b331c50643d092c94021a07e8a8fb
Ahmed Kordy <ahmed@kordy.info>
2015-07-29 15:25:06 +0200

    Added admin index page

---
+++ src/controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Kordy\Ticketit\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller {
+
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('Ticketit::admin.index');
+    }
+}

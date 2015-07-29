8c816b632c1b795fbb3b2b8e9ebd07f446734386
Ahmed Kordy <ahmed@kordy.info>
2015-07-29 15:24:47 +0200

    Added admin nav

---
+++ src/views/admin/nav.blade.php
@@ -0,0 +1,15 @@
+<div class="panel panel-default">
+    <div class="panel-body">
+        <ul class="nav nav-pills">
+            <li role="presentation" class="{!! Request::is(config('ticketit.admin_route')) ? "active" : "" !!}">
+                <a href="/{{ config('ticketit.admin_route') }}"> Dashboard </a>
+            </li>
+            <li role="presentation" class="{!! Request::is(config('ticketit.admin_route')."/status*") ? "active" : "" !!}">
+                <a href="/{{ config('ticketit.admin_route') }}/status"> Statuses </a>
+            </li>
+            <li role="presentation"  class="{!! Request::is(config('ticketit.admin_route')."/priority*") ? "active" : "" !!}">
+                <a href="/{{ config('ticketit.admin_route') }}/priority"> Priorities </a>
+            </li>
+        </ul>
+    </div>
+</div>

70a72211dd5b00d5f2979e68411e88ea75835fd9
Ahmed Kordy <ahmed@kordy.info>
2015-07-30 10:04:15 +0200

    Fixing config routes

--- src/config/ticketit.php
+++ src/config/ticketit.php
@@ -6,13 +6,13 @@ return [
     * Ticketit main route: Where to load the ticket system (ex. http://url/tickets)
     * Default: /ticket
     */
-    'main_route' => '/tickets',
+    'main_route' => 'tickets',
 
     /*
     * Ticketit admin route: Where to load the ticket administration dashboard (ex. http://url/tickets-admin)
     * Default: /ticket
     */
-    'admin_route' => '/tickets-admin',
+    'admin_route' => 'tickets-admin',
 
     /*
     * Template adherence: The master blade template to be extended

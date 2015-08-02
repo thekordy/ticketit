c61e9327549197b82e64f34e9a1ea4c77f11adf2
Ahmed Kordy <ahmed@kordy.info>
2015-08-02 15:06:46 +0300

    Agent categories form

--- src/controllers/AgentsController.php
+++ src/controllers/AgentsController.php
@@ -1,108 +1,81 @@
 <?php
 namespace Kordy\Ticketit\Controllers;
 
-use Kordy\Ticketit\Models\Agent;
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use Kordy\Ticketit\Models\Category;
+use Kordy\Ticketit\Models\Agent;
 
 class AgentsController extends Controller {
 
-    public function __construct() {
-        $this->middleware('auth');
-    }
+	public function __construct() {
+		$this->middleware('auth');
+	}
 
-    public function index()
-    {
-        $agents = Agent::where('ticketit_agent', 1)->get();
-        return view('Ticketit::admin.agent.index', compact('agents'));
-    }
+	public function index() {
+		$agents = Agent::where('ticketit_agent', 1)->get();
+		return view('Ticketit::admin.agent.index', compact('agents'));
+	}
 
-    public function create()
-    {
-        $users = Agent::all();
-        return view('Ticketit::admin.agent.create', compact('users'));
-    }
+	public function create() {
+		$users = Agent::all();
+		return view('Ticketit::admin.agent.create', compact('users'));
+	}
 
-    public function store(Request $request)
-    {
-        $agents_list = $this->addAgents($request->input('agents'));
-        $agents_names = implode(',', $agents_list);
+	public function store(Request $request) {
+		$agents_list = $this->addAgents($request->input('agents'));
+		$agents_names = implode(',', $agents_list);
 
-        Session::flash('status', "Agents $agents_names are added to agents");
+		Session::flash('status', "Agents $agents_names are added to agents");
 
-        return redirect()->action('\Kordy\Ticketit\Controllers\AgentsController@index');
-    }
+		return redirect()->action('\Kordy\Ticketit\Controllers\AgentsController@index');
+	}
 
-    public function update($id, Request $request)
-    {
-        $this->joinCategory($id, $request);
+	public function update($id, Request $request) {
+		$form_cats = ($request->input('agent_cats') == null) ? [] : $request->input('agent_cats');
+		$agent = Agent::find($id);
+		$agent->categories()->sync($form_cats);
 
-        Session::flash('status', "Joint categories successfully");
+		Session::flash('status', "Joint categories successfully");
 
-        return redirect()->action('\Kordy\Ticketit\Controllers\AgentsController@index');
-    }
+		return redirect()->action('\Kordy\Ticketit\Controllers\AgentsController@index');
+	}
 
-    public function destroy($id)
-    {
-        $agent = $this->removeAgent($id);
+	public function destroy($id) {
+		$agent = $this->removeAgent($id);
 
-        Session::flash('status', "Agents $agent->name is removed from agents team");
+		Session::flash('status', "Agents $agent->name is removed from agents team");
 
-        return redirect()->action('\Kordy\Ticketit\Controllers\AgentsController@index');
-    }
+		return redirect()->action('\Kordy\Ticketit\Controllers\AgentsController@index');
+	}
 
-    /**
-     * Assign users as agents
-     *
-     * @param $user_ids
-     * @return array
-     */
-    public function addAgents($user_ids)
-    {
-        $users = Agent::find($user_ids);
-        foreach ($users as $user) {
-            $user->ticketit_agent = true;
-            $user->save();
-            $users_list[] = $user->name;
-        }
-        return $users_list;
-    }
+	/**
+	 * Assign users as agents
+	 *
+	 * @param $user_ids
+	 * @return array
+	 */
+	public function addAgents($user_ids) {
+		$users = Agent::find($user_ids);
+		foreach ($users as $user) {
+			$user->ticketit_agent = true;
+			$user->save();
+			$users_list[] = $user->name;
+		}
+		return $users_list;
+	}
 
-    /**
-     * Remove user from the agents
-     * @param $id
-     * @return mixed
-     */
-    public function removeAgent($id)
-    {
-        $agent = Agent::find($id);
-        $agent->ticketit_agent = false;
-        $agent->save();
+	/**
+	 * Remove user from the agents
+	 * @param $id
+	 * @return mixed
+	 */
+	public function removeAgent($id) {
+		$agent = Agent::find($id);
+		$agent->ticketit_agent = false;
+		$agent->save();
 
-        return $agent;
-    }
-
-    /**
-     * @param $id
-     * @param Request $request
-     */
-    public function joinCategory($id, Request $request)
-    {
-        $agent_categories_ids = $request->input('agent_cats');
-        $agent = Agent::find($id);
-        $all_categories_ids = Category::lists('id');
-        foreach($all_categories as $cat) {
-            if(in_array($cat, $agent_categories)) {
-
-            }
-        }
-
-        $agent->categories()->attach($categories);
-    }
-
+		return $agent;
+	}
 
 }

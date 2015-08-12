baa41300ab95c63fce6a63d307a4ed04b45e721e
Ahmed Kordy <ahmed@kordy.info>
2015-08-12 13:36:02 +0200

    residing the ticket content into "well" box

--- src/views/tickets/partials/ticket_body.blade.php
+++ src/views/tickets/partials/ticket_body.blade.php
@@ -13,39 +13,41 @@
                     @endif
                 </span>
             </h2>
-            <hr>
-            <div class="col-md-12">
-                <div class="col-md-6">
-                    <p> <strong>Owner</strong>: {{ $ticket->user->name }}</p>
-                    <p>
-                        <strong>Status</strong>:
-                        <span style="color: {{ $ticket->status->color }}">
-                            {{ $ticket->status->name }}
-                        </span>
-                    </p>
-                    <p>
-                        <strong>Priority</strong>:
-                        <span style="color: {{ $ticket->priority->color }}">
-                            {{ $ticket->priority->name }}
-                        </span>
-                    </p>
-                </div>
-                <div class="col-md-6">
-                    <p> <strong>Responsible</strong>: {{ $ticket->agent->name }}</p>
-                    <p>
-                        <strong>Category</strong>:
-                        <span style="color: {{ $ticket->category->color }}">
-                            {{ $ticket->category->name }}
-                        </span>
-                    </p>
-                    <p> <strong>Created</strong>: {{ $ticket->created_at->diffForHumans() }}</p>
-                    <p> <strong>Last Update</strong>: {{ $ticket->updated_at->diffForHumans() }}</p>
+            <div class="panel well well-sm">
+                <div class="panel-body">
+                    <div class="col-md-12">
+                        <div class="col-md-6">
+                            <p> <strong>Owner</strong>: {{ $ticket->user->name }}</p>
+                            <p>
+                                <strong>Status</strong>:
+                                <span style="color: {{ $ticket->status->color }}">
+                                    {{ $ticket->status->name }}
+                                </span>
+                            </p>
+                            <p>
+                                <strong>Priority</strong>:
+                                <span style="color: {{ $ticket->priority->color }}">
+                                    {{ $ticket->priority->name }}
+                                </span>
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p> <strong>Responsible</strong>: {{ $ticket->agent->name }}</p>
+                            <p>
+                                <strong>Category</strong>:
+                                <span style="color: {{ $ticket->category->color }}">
+                                    {{ $ticket->category->name }}
+                                </span>
+                            </p>
+                            <p> <strong>Created</strong>: {{ $ticket->created_at->diffForHumans() }}</p>
+                            <p> <strong>Last Update</strong>: {{ $ticket->updated_at->diffForHumans() }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <br>
+
             <div class="col-md-12">
-                <hr>
-                <p> {{ nl2br(e($ticket->content)) }} </p>
+                <p> {!! nl2br(e($ticket->content))  !!} </p>
             </div>
         </div>
     </div>

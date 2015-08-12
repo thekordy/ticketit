c437c04f9b79eadab2cb4d23e368d3e143cf8d2d
Ahmed Kordy <ahmed@kordy.info>
2015-08-12 13:45:52 +0200

    Comments title

--- src/views/tickets/show.blade.php
+++ src/views/tickets/show.blade.php
@@ -3,6 +3,8 @@
 @section('content')
         @include('Ticketit::nav')
         @include('Ticketit::tickets.partials.ticket_body')
+        <br>
+        <h2>Comments</h2>
         @include('Ticketit::tickets.partials.comments')
         @include('Ticketit::tickets.partials.comment_form')
 

1949b4130aa789423e23c1a73e0284c2d6286610
kordy <ahmed@kordy.info>
2015-10-02 21:47:07 +0200

    Merge branch 'v0.1.0-localization' into v0.1.0
    Conflicts:
    	src/TicketitServiceProvider.php
    	src/Views/nav.blade.php
    	src/Views/tickets/create.blade.php

--- src/Views/nav.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="panel panel-default">
-    <div class="panel-body">
-        <ul class="nav nav-pills">
-            <li role="presentation" class="{!! Request::is(config('ticketit.main_route')) ? "active" : "" !!}">
-                <a href="/{{ config('ticketit.main_route') }}">My Tickets </a>
-            </li>
-        </ul>
-    </div>
-</div>

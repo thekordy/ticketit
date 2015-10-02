45eadfaf94387dde0d396c6a464d0f14478cecd0
kordy <ahmed@kordy.info>
2015-10-02 20:52:08 +0200

    translating create form #5 #21 #22 .. thanks to  #16

--- src/Views/tickets/create.blade.php
+++ src/Views/tickets/create.blade.php
@@ -1,5 +1,5 @@
 @extends($master)
-@section('page', 'New Ticket Form')
+@section('page', trans('ticketit::messages.page_new_ticket_form'))
 
 @section('content')
     @include('ticketit::shared.header')
@@ -9,30 +9,34 @@
                         'method' => 'POST',
                         'class' => 'form-horizontal'
                         ]) !!}
-            <legend>Create New Ticket</legend>
+            <legend>{{ trans('ticketit::messages.create_new_ticket') }}</legend>
             <div class="form-group">
-                {!! Form::label('subject', 'Subject:', ['class' => 'col-lg-2 control-label']) !!}
+                {!! Form::label('subject', trans('ticketit::messages.create_new_ticket_subject'),
+                    ['class' => 'col-lg-2 control-label']) !!}
                 <div class="col-lg-10">
                     {!! Form::text('subject', null, ['class' => 'form-control']) !!}
-                    <span class="help-block">A brief of your issue</span>
+                    <span class="help-block">{{ trans('ticketit::messages.create_new_ticket_brief_issue') }}</span>
                 </div>
             </div>
             <div class="form-group">
-                {!! Form::label('content', 'Description:', ['class' => 'col-lg-2 control-label']) !!}
+                {!! Form::label('content', trans('ticketit::messages.create_new_ticket_description'),
+                    ['class' => 'col-lg-2 control-label']) !!}
                 <div class="col-lg-10">
                     {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '5']) !!}
-                    <span class="help-block">Describe your issue here in details</span>
+                    <span class="help-block">{{ trans('ticketit::messages.create_new_ticket_describe_issue') }}</span>
                 </div>
             </div>
             <div class="form-inline row">
                 <div class="form-group col-lg-4">
-                    {!! Form::label('priority', 'Priority:', ['class' => 'col-lg-6 control-label']) !!}
+                    {!! Form::label('priority', trans('ticketit::messages.create_new_ticket_priority'),
+                        ['class' => 'col-lg-6 control-label']) !!}
                     <div class="col-lg-6">
                         {!! Form::select('priority_id', $priorities, null, ['class' => 'form-control']) !!}
                     </div>
                 </div>
                 <div class="form-group col-lg-4">
-                    {!! Form::label('category', 'Category:', ['class' => 'col-lg-6 control-label']) !!}
+                    {!! Form::label('category', trans('ticketit::messages.create_new_ticket_category'),
+                        ['class' => 'col-lg-6 control-label']) !!}
                     <div class="col-lg-6">
                         {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
                     </div>
@@ -41,8 +45,9 @@
             <br>
             <div class="form-group">
                 <div class="col-lg-10 col-lg-offset-2">
-                    {!! link_to_route(config('ticketit.main_route').'.index', 'Back', null, ['class' => 'btn btn-default']) !!}
-                    {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+                    {!! link_to_route(config('ticketit.main_route').'.index',
+                        trans('ticketit::messages.create_new_ticket_back'), null, ['class' => 'btn btn-default']) !!}
+                    {!! Form::submit(trans('ticketit::messages.create_new_ticket_submit'), ['class' => 'btn btn-primary']) !!}
                 </div>
             </div>
         {!! Form::close() !!}

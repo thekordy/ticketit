958c0cc5e016881c8d1fcbcd21015a66c9a5e14e
Brian Royer <brian@fusiondesign.me>
2015-10-03 22:36:30 -0400

    Removing useless old index checks. Handled in Controller.

--- src/TicketitServiceProvider.php
+++ src/TicketitServiceProvider.php
@@ -1,102 +1,98 @@
 <?php
 namespace Kordy\Ticketit;
 
-use Kordy\Ticketit\Models\Agent;
 use Collective\Html\FormFacade as Form;
 use Illuminate\Support\ServiceProvider;
 use Kordy\Ticketit\Controllers\NotificationsController;
+use Kordy\Ticketit\Models\Agent;
 use Kordy\Ticketit\Models\Comment;
 use Kordy\Ticketit\Models\Ticket;
 
-class TicketitServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        // Send the Agent User model to the view under $u
-        view()->composer('*', function($view) {
-            if( auth()->check() )
-            {
-                $u = Agent::find( auth()->user()->id );
-                $view->with( 'u', $u );
-            }
-        });
-        // Adding HTML5 color picker to form elements
-        Form::macro('custom', function($type, $name, $value = "#000000", $options = [])
-        {
-            $field = $this->input($type, $name, $value, $options);
-            return $field;
-        });
+class TicketitServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		// Send the Agent User model to the view under $u
+		view()->composer('*', function ($view) {
+			if (auth()->check()) {
+				$u = Agent::find(auth()->user()->id);
+				$view->with('u', $u);
+			}
+		});
+		// Adding HTML5 color picker to form elements
+		Form::macro('custom', function ($type, $name, $value = "#000000", $options = []) {
+			$field = $this->input($type, $name, $value, $options);
+			return $field;
+		});
 
-        // Passing to views the master view value from the setting file
-        view()->composer('ticketit::*', function ($view) {
-            $master = config('ticketit.master_template');
-            $view->with(compact('master'));
-        });
+		// Passing to views the master view value from the setting file
+		view()->composer('ticketit::*', function ($view) {
+			$master = config('ticketit.master_template');
+			$view->with(compact('master'));
+		});
 
-        // Send notification when new comment is added
-        Comment::creating(function ($comment) {
-            if (config('ticketit.comment_notification') == 'yes') {
-                $notification = new NotificationsController();
-                $notification->newComment($comment);
-            }
+		// Send notification when new comment is added
+		Comment::creating(function ($comment) {
+			if (config('ticketit.comment_notification') == 'yes') {
+				$notification = new NotificationsController();
+				$notification->newComment($comment);
+			}
 
-        });
+		});
 
-        // Send notification when ticket status is modified
-        Ticket::updating(function ($modified_ticket) {
-            if (config('ticketit.status_notification') == 'yes') {
-                $original_ticket = Ticket::find($modified_ticket->id);
-                if ($original_ticket->status->id != $modified_ticket->status->id || $original_ticket->completed_at != $modified_ticket->completed_at) {
-                    $notification = new NotificationsController();
-                    $notification->ticketStatusUpdated($modified_ticket, $original_ticket);
-                }
-            }
-            if (config('ticketit.assigned_notification') == 'yes') {
-                $original_ticket = Ticket::find($modified_ticket->id);
-                if ($original_ticket->agent->id != $modified_ticket->agent->id) {
-                    $notification = new NotificationsController();
-                    $notification->ticketAgentUpdated($modified_ticket, $original_ticket);
-                }
-            }
-            return true;
-        });
+		// Send notification when ticket status is modified
+		Ticket::updating(function ($modified_ticket) {
+			if (config('ticketit.status_notification') == 'yes') {
+				$original_ticket = Ticket::find($modified_ticket->id);
+				if ($original_ticket->status->id != $modified_ticket->status->id || $original_ticket->completed_at != $modified_ticket->completed_at) {
+					$notification = new NotificationsController();
+					$notification->ticketStatusUpdated($modified_ticket, $original_ticket);
+				}
+			}
+			if (config('ticketit.assigned_notification') == 'yes') {
+				dd($original_ticket);
+				$original_ticket = Ticket::find($modified_ticket->id);
+				if ($original_ticket->agent->id != $modified_ticket->agent->id) {
+					$notification = new NotificationsController();
+					$notification->ticketAgentUpdated($modified_ticket, $original_ticket);
+				}
+			}
+			return true;
+		});
 
-        // Send notification when ticket status is modified
-        Ticket::created(function ($ticket) {
-            if (config('ticketit.assigned_notification') == 'yes') {
-                $notification = new NotificationsController();
-                $notification->newTicketNotifyAgent($ticket);
-            }
-            return true;
-        });
+		// Send notification when ticket status is modified
+		Ticket::created(function ($ticket) {
+			if (config('ticketit.assigned_notification') == 'yes') {
+				$notification = new NotificationsController();
+				$notification->newTicketNotifyAgent($ticket);
+			}
+			return true;
+		});
 
-        $this->loadTranslationsFrom(__DIR__.'/Translations', 'ticketit');
+		$this->loadTranslationsFrom(__DIR__ . '/Translations', 'ticketit');
 
-        $this->loadViewsFrom(__DIR__.'/Views', 'ticketit');
+		$this->loadViewsFrom(__DIR__ . '/Views', 'ticketit');
 
-        $this->publishes([__DIR__.'/Views' => base_path('resources/views/vendor/ticketit')], 'views');
-        $this->publishes([__DIR__.'/Config/ticketit.php' => config_path('ticketit.php')], 'config');
-        $this->publishes([__DIR__.'/Translations' => base_path('resources/lang/vendor/ticketit')], 'lang');
+		$this->publishes([__DIR__ . '/Views' => base_path('resources/views/vendor/ticketit')], 'views');
+		$this->publishes([__DIR__ . '/Config/ticketit.php' => config_path('ticketit.php')], 'config');
+		$this->publishes([__DIR__ . '/Translations' => base_path('resources/lang/vendor/ticketit')], 'lang');
 
-    }
+	}
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        // Merging config files, no need for to publish the config file at update, only if needed to override configs
-        $this->mergeConfigFrom(
-            __DIR__.'/Config/ticketit.php', 'ticketit'
-        );
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		// Merging config files, no need for to publish the config file at update, only if needed to override configs
+		$this->mergeConfigFrom(
+			__DIR__ . '/Config/ticketit.php', 'ticketit'
+		);
 
-        include __DIR__.'/routes.php';
-    }
+		include __DIR__ . '/routes.php';
+	}
 }
--- src/Views/tickets/index.blade.php
+++ src/Views/tickets/index.blade.php
@@ -7,26 +7,9 @@
     </div>
 
     <div class="panel-body">
-        @if ( ! $u->getTickets()->count() && Request::is(config('ticketit.main_route').'/complete'))
-            <h3 class="text-center"> There are no complete tickets</h3>
-            <br>
-            <h4 class="text-center">
-                <small>Be sure to check Active Tickets if you cannot find your ticket.</small>
-            </h4>
-        @elseif( ! $u->getTickets()->count())
-            <h3 class="text-center"> There are no active tickets,
-                {!! link_to_route(config('ticketit.main_route').'.create', 'create new ticket') !!}
-            </h3>
-            <br>
-            <h4 class="text-center">
-                <small>Be sure to check Complete Tickets if you cannot find your ticket.</small>
-            </h4>
-        @else
-            <div id="message"></div>
+        <div id="message"></div>
 
-            @include('ticketit::tickets.partials.datatable')
-
-        @endif
+        @include('ticketit::tickets.partials.datatable')
     </div>
 
 </div>

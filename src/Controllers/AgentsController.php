d1da60880526807c7808492255c158ef26ec8658
Brian Royer <brian@fusiondesign.me>
2015-10-09 01:21:34 -0400

    Fix missing dependency.

--- src/Controllers/AgentsController.php
+++ src/Controllers/AgentsController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Kordy\Ticketit\Models\Agent;
+use Kordy\Ticketit\Models\Setting;
 
 class AgentsController extends Controller
 {

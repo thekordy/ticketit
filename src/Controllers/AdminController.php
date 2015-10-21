4dc9cc16d13109fdf01057dd952f36d7ae51361d
kordy <ahmed@kordy.info>
2015-10-22 00:55:19 +0200

    Dashboard Total, closed, and open counters in general and per category (Still under development) .. #68

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -5,12 +5,34 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Kordy\Ticketit\Models\Category;
+use Kordy\Ticketit\Models\Ticket;
 
 class AdminController extends Controller {
 
     public function index()
     {
-        return view('ticketit::admin.index');
+        $tickets_count = Ticket::count();
+        $open_tickets_count = Ticket::whereNull('completed_at')->count();
+        $closed_tickets_count = Ticket::whereNotNull('completed_at')->count();
+
+        // Per Category
+        $categories = Category::all();
+        $categories_count = [];
+        foreach ($categories as $category) {
+            $categories_count[$category->name]['open'] = $category->tickets()->whereNull('completed_at')->count();
+            $categories_count[$category->name]['closed'] = $category->tickets()->whereNotNull('completed_at')->count();
+            $categories_count[$category->name]['color'] = $category->color;
+        }
+
+        return view(
+            'ticketit::admin.index',
+            compact(
+                'open_tickets_count',
+                'closed_tickets_count',
+                'tickets_count',
+                'categories_count'
+            ));
     }
 
 }
--- src/Views/admin/index.blade.php
+++ src/Views/admin/index.blade.php
@@ -6,4 +6,74 @@
 
 @section('content')
     @include('ticketit::shared.header')
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3" style="font-size: 5em;">
+                            <i class="glyphicon glyphicon-th"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <h1>{{ $tickets_count }}</h1>
+                            <div>Total Tickets</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3" style="font-size: 5em;">
+                            <i class="glyphicon glyphicon-wrench"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <h1>{{ $open_tickets_count }}</h1>
+                            <div>Open Tickets</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3" style="font-size: 5em;">
+                            <i class="glyphicon glyphicon-thumbs-up"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <h1>{{ $closed_tickets_count }}</h1>
+                            <span>Closed Tickets</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+    @foreach($categories_count as $category_name => $category_counts)
+        <div class="col-lg-2 col-md-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading" style="background-color: {{ $category_counts['color'] }}">
+                    <div class="row">
+                        <div class="col-xs-6 text-right">
+                            <h1> {{ $category_counts['open'] }} </h1>
+                            <div>Open</div>
+                        </div>
+                        <div class="col-xs-6" style="border-left: 4px solid #FFFFFF">
+                            <h1>{{ $category_counts['closed'] }}</h1>
+                            <div>Closed</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer text-center">
+                    {{ $category_name }}
+                </div>
+            </div>
+        </div>
+    @endforeach
+    </div>
 @stop

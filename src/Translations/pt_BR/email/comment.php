cea3e162e56359f9126b8f5518208f524e472a0e
Anderson de Oliveira Machado <andersondeoliveiramachado@gmail.com>
2015-11-05 11:49:46 -0200

    Translation for Brazilian Portuguese.

---
+++ src/Translations/pt_BR/email/comment.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'data' => '
+        <b>:name</b> Comentou sobre o chamdo: <b>:subject</b><br>
+        <b>Categoria do Chamado:</b> :category - <b>Status:</b> :status<br>
+    <br>
+        <div><b>:comment</b></div><br>
+    ',
+];
---
+++ src/Translations/pt_BR/email/globals.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'assigned'    => 'Chamado Atribuido',
+    'comment'     => 'Novo Comentário',
+    'status'      => 'Status alterado',
+    'transfer'    => 'Chamado Transferido',
+    'view-ticket' => 'Click aqui pra ver seu chamado.',
+];

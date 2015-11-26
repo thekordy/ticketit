bc8b5b69144bb92e52e2ab198b34a7096587da9e
datune <hauser.j@gmail.com>
2015-11-26 09:57:52 +0100

    German translation

--- src/Translations/de/email/transfer.php
+++ src/Translations/de/email/transfer.php
@@ -2,6 +2,6 @@
 
 return [
 
-'data' => '<b>:name</b> has transferred the ticket "<b>:subject</b>" from :agent in :old_category to you in :new_category<br>',  
+'data' => '<b>:name</b> hat das Ticket "<b>:subject</b>" von :agent in :old_category zu Dir in :new_category verschoben<br>',  
 
 ];

c2e462f8b4170467a161a8bd1699c9d050e37ee0
tmcdon89 <tom@Toms-iMac.local>
2015-12-14 09:53:57 -0600

    Added private comments and improved work time output
    Private comments will not be emailed to the client and are only visible
    by admins and agents.
    Changed comment order to show newest comments first
    Changed total work time to account for hours and minutes instead of
    just minutes
    Added color to non-private comments to distinguish

--- src/Views/tickets/partials/comments.blade.php
+++ src/Views/tickets/partials/comments.blade.php
@@ -1,9 +1,19 @@
 @if(!$comments->isEmpty())
     @foreach($comments as $comment)
-        <div class="panel {!! $comment->user->tickets_role ? "panel-info" : "panel-default" !!}">
-            <div class="panel-heading">
-                <h3 class="panel-title">
-                    {!! $comment->user->name !!}
+        @if( (!$u->isAgent() || !$u->isAdmin()) && $comment->private == 1 )
+            <?php continue; ?>
+        @endif
+        @if ($comment->private == 1)
+            <div class="panel {!! $comment->user->tickets_role ? "panel-info" : "panel-default" !!}">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        {!! $comment->user->name !!} - {{ trans('ticketit::lang.private') }}
+        @else
+            <div class="panel {!! $comment->user->tickets_role ? "panel-info" : "panel-primary" !!}">
+                <div class="panel-heading">
+                     <h3 class="panel-title">
+                        {!! $comment->user->name !!}
+        @endif
                     <span class="pull-right"> {!! $comment->created_at->diffForHumans() !!} </span>
                 </h3>
             </div>

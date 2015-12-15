efd7461d85718c25beae5cfb68f184bf186e19d1
Ahmed Kordy <ahmed@kordy.info>
2015-12-15 23:12:44 +0200

    Imporved dashboard performance from 119 queries to only 24, and half of the load time. #94 #71

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -14,40 +14,34 @@ class DashboardController extends Controller {
 
     public function index($indicator_period = 2)
     {
-        $tickets_count = Ticket::count();
-        $open_tickets_count = Ticket::whereNull('completed_at')->count();
-        $closed_tickets_count = Ticket::whereNotNull('completed_at')->count();
+        $tickets_total = Ticket::all();
+        $tickets_count = $tickets_total->count();
+        $open_tickets_count = $tickets_total->where('completed_at', null)->count();
+        $closed_tickets_count = $tickets_count - $open_tickets_count;
 
         // Per Category pagination
-        $categories = Category::paginate(10,['*'],'cat_page');
+        $categories = Category::with('tickets')->paginate(10,['*'],'cat_page');
 
         // Total tickets counter per category for google pie chart
-        $categories_all = Category::all();
-        $categories_share = [];
-        foreach ($categories_all as $cat) {
-            $categories_share[$cat->name] = $cat->tickets()->count();
-        }
+        $categories_all = Category::with('tickets')->get();
 
         // Total tickets counter per agent for google pie chart
-        $agents_all = Agent::agents();
-        $agents_share = [];
-        foreach ($agents_all as $agent_single) {
-            $agents_share[$agent_single->name] = $agent_single->agentTickets(false)->count() + $agent_single->agentTickets(true)->count();
-        }
+        $agents_all = Agent::agents()->with('agentTicketsTotal')->get();
 
         // Per Agent
-        $agents = Agent::agents(10);
+        $agents = Agent::with('agentTicketsTotal')->agents(10);
 
         // Per User
-        $users = Agent::users(10);
+        $users = Agent::with('userTicketsTotal')->users(10);
 
-        // Per Category performance data 
+        // Per Category performance data
         $ticketController = new TicketsController(new Ticket, new Agent);
-        $monthly_performance = $ticketController->monthlyPerfomance($indicator_period);
+        $monthly_performance = $ticketController->monthlyPerfomance($indicator_period, $categories_all);
 
         return view(
             'ticketit::admin.index',
             compact(
+                'tickets_total',
                 'open_tickets_count',
                 'closed_tickets_count',
                 'tickets_count',
@@ -55,8 +49,8 @@ class DashboardController extends Controller {
                 'agents',
                 'users',
                 'monthly_performance',
-                'categories_share',
-                'agents_share'
+                'categories_all',
+                'agents_all'
             ));
     }
 

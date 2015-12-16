090ccf60040b26d5e99d84fb0e9454e13f3f7b90
tmcdon89 <tom@Toms-iMac.local>
2015-12-16 13:29:15 -0600

    Correct Migration file for attachments addition

--- src/Migrations/2015_12_14_173904_add_attachments_table.php
+++ src/Migrations/2015_12_14_173904_add_attachments_table.php
@@ -16,15 +16,16 @@ class AddAttachmentsTable extends Migration
 	{
 		$table->increments('id');
 		$table->string('filename');
+		$table->string('filepath');
 		$table->string('mime');
 		$table->string('original_filename');
-		$table->integer('ticket_id')->unsigned;
+		$table->integer('ticket_id')->unsigned();
 		$table->integer('comment_id')->unsigned()->nullable();
+		$table->timestamps();
 		$table->index('ticket_id');
 		$table->index('comment_id');
 		$table->foreign('ticket_id')->references('id')->on('ticketit')->onUpdate('RESTRICT')->onDelete('RESTRICT');
 		$table->foreign('comment_id')->references('id')->on('ticketit_comments')->onUpdate('RESTRICT')->onDelete('RESTRICT');
-		$table->timestamps();
 	});
     }
 
--- src/Models/Attachment.php
+++ src/Models/Attachment.php
@@ -16,15 +16,10 @@ class Attachment extends Model {
     public function comment() {
         return $this->belongsTo('Kordy\Ticketit\Models\Comment', 'comment_id');
     }
-    /**
-     * Get related attachment
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
+
     public function ticket() {
         return $this->belongsto('Kordy\Ticketit\Models\Ticket', 'ticket_id');
     }
-
     /**
      * Get comment owner
      *

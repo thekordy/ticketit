bd4c8f96635bdff81019dd161ce0ea6bb2dc9ebd
balping <balping314@gmail.com>
2015-12-28 23:11:59 +0100

    Added Route facade
    Maybe I have something misconfigured, but I need `Route::` before each get, post, resource, etc. function call. I'm using Laravel 5.2 by the way

--- src/routes.php
+++ src/routes.php
@@ -2,28 +2,28 @@
 
 Route::group(['middleware' => 'auth'], function () use ($main_route) {
 	//Ticket public route
-	get("$main_route/complete", 'Kordy\Ticketit\Controllers\TicketsController@indexComplete')
+	Route::get("$main_route/complete", 'Kordy\Ticketit\Controllers\TicketsController@indexComplete')
 		->name("$main_route-complete");
-	get("$main_route/data/{id?}", 'Kordy\Ticketit\Controllers\TicketsController@data')
+	Route::get("$main_route/data/{id?}", 'Kordy\Ticketit\Controllers\TicketsController@data')
 		->name("$main_route.data");
-	resource($main_route, 'Kordy\Ticketit\Controllers\TicketsController');
+	Route::resource($main_route, 'Kordy\Ticketit\Controllers\TicketsController');
 
 	//Ticket Comments public route
-	resource("$main_route-comment", 'Kordy\Ticketit\Controllers\CommentsController');
+	Route::resource("$main_route-comment", 'Kordy\Ticketit\Controllers\CommentsController');
 
 	//Ticket complete route for permitted user.
-	get("$main_route/{id}/complete", 'Kordy\Ticketit\Controllers\TicketsController@complete')
+	Route::get("$main_route/{id}/complete", 'Kordy\Ticketit\Controllers\TicketsController@complete')
 		->name("$main_route.complete");
 
 	//Ticket reopen route for permitted user.
-	get("$main_route/{id}/reopen", 'Kordy\Ticketit\Controllers\TicketsController@reopen')
+	Route::get("$main_route/{id}/reopen", 'Kordy\Ticketit\Controllers\TicketsController@reopen')
 		->name("$main_route.reopen");
 });
 
 Route::group(['middleware' => 'Kordy\Ticketit\Middleware\IsAgentMiddleware'], function () use ($main_route) {
 
 	//API return list of agents in particular category
-	get("$main_route/agents/list/{category_id?}/{ticket_id?}", [
+	Route::get("$main_route/agents/list/{category_id?}/{ticket_id?}", [
 		'as' => $main_route . 'agentselectlist',
 		'uses' => 'Kordy\Ticketit\Controllers\TicketsController@agentSelectList',
 	]);
@@ -31,30 +31,30 @@ Route::group(['middleware' => 'Kordy\Ticketit\Middleware\IsAgentMiddleware'], fu
 
 Route::group(['middleware' => 'Kordy\Ticketit\Middleware\IsAdminMiddleware'], function () use ($admin_route) {
 	//Ticket admin index route (ex. http://url/tickets-admin/)
-	get("$admin_route/indicator/{indicator_period?}", [
+	Route::get("$admin_route/indicator/{indicator_period?}", [
 			'as' => 'dashboard.indicator',
 			'uses' => 'Kordy\Ticketit\Controllers\DashboardController@index'
 	]);
-	get($admin_route, 'Kordy\Ticketit\Controllers\DashboardController@index');
+	Route::get($admin_route, 'Kordy\Ticketit\Controllers\DashboardController@index');
 
 	//Ticket statuses admin routes (ex. http://url/tickets-admin/status)
-	resource("$admin_route/status", 'Kordy\Ticketit\Controllers\StatusesController');
+	Route::resource("$admin_route/status", 'Kordy\Ticketit\Controllers\StatusesController');
 
 	//Ticket priorities admin routes (ex. http://url/tickets-admin/priority)
-	resource("$admin_route/priority", 'Kordy\Ticketit\Controllers\PrioritiesController');
+	Route::resource("$admin_route/priority", 'Kordy\Ticketit\Controllers\PrioritiesController');
 
 	//Agents management routes (ex. http://url/tickets-admin/agent)
-	resource("$admin_route/agent", 'Kordy\Ticketit\Controllers\AgentsController');
+	Route::resource("$admin_route/agent", 'Kordy\Ticketit\Controllers\AgentsController');
 
 	//Agents management routes (ex. http://url/tickets-admin/agent)
-	resource("$admin_route/category", 'Kordy\Ticketit\Controllers\CategoriesController');
+	Route::resource("$admin_route/category", 'Kordy\Ticketit\Controllers\CategoriesController');
 
 	//Settings configuration routes (ex. http://url/tickets-admin/configuration)
-	resource("$admin_route/configuration", 'Kordy\Ticketit\Controllers\ConfigurationsController');
+	Route::resource("$admin_route/configuration", 'Kordy\Ticketit\Controllers\ConfigurationsController');
 
     //Administrators configuration routes (ex. http://url/tickets-admin/administrators)
-    resource("$admin_route/administrator", 'Kordy\Ticketit\Controllers\AdministratorsController');
+    Route::resource("$admin_route/administrator", 'Kordy\Ticketit\Controllers\AdministratorsController');
 
 	//Tickets demo data route (ex. http://url/tickets-admin/demo-seeds/)
-	// get("$admin_route/demo-seeds", 'Kordy\Ticketit\Controllers\InstallController@demoDataSeeder');
+	// Route::get("$admin_route/demo-seeds", 'Kordy\Ticketit\Controllers\InstallController@demoDataSeeder');
 });

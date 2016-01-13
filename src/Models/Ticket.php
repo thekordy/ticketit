cee6987fcc8c8df488f387d28ebe84e40441eced
Ahmed Kordy <ahmed@kordy.info>
2016-01-13 21:15:34 +0200

    responding to #105 .. fix completed tickets of agent

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -190,6 +190,8 @@ class Ticket extends Model
      */
     public function scopeAgentUserTickets($query, $id)
     {
-        return $query->where('agent_id', $id)->orWhere('user_id', $id);
+        return $query->where(function($subquery) use ($id) {
+            $subquery->where('agent_id', $id)->orWhere('user_id', $id);
+        });
     }
 }

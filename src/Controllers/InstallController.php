881c81e7182c21e291cdc03cbcb0d356303e59ac
Ahmed Kordy <ahmed@kordy.info>
2016-01-13 22:42:55 +0200

    enhanced new migrations lookup at startup

--- src/Controllers/InstallController.php
+++ src/Controllers/InstallController.php
@@ -13,12 +13,12 @@ use Kordy\Ticketit\Seeds\TicketitTableSeeder;
 
 class InstallController extends Controller
 {
-    protected $migrations_tables = [
-        '2015_07_22_115516_create_ticketit_tables',
-        '2015_07_22_123254_alter_users_table',
-        '2015_09_29_123456_add_completed_at_column_to_ticketit_table',
-        '2015_10_08_123457_create_settings_table'
-    ];
+    protected $migrations_tables = [];
+
+    public function __construct()
+    {
+        $this->migrations_tables = \File::files('vendor/kordy/ticketit/src/Migrations');
+    }
 
     public function publicAssets() {
         $public = $this->allFilesList(public_path('vendor/ticketit'));
@@ -165,6 +165,26 @@ class InstallController extends Controller
             }
         }
         return $inactiveMigrations;
+    }
+
+    /**
+     * Check if all Ticketit Package settings that were not installed to setting table
+     * @return boolean
+     */
+    public function inactiveSettings()
+    {
+        $seeder = new SettingsTableSeeder();
+
+        // Package Settings
+        $installedSettings = DB::table('ticketit_settings')->get();
+
+        // Application active migrations
+        $defaultSettings = $seeder->getDefaults();
+
+        if (count($installedSettings) == count($defaultSettings))
+            return false;
+
+        return true;
     }
 
     /**

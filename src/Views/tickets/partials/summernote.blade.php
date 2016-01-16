df879061d9b5561702c1b79e19bfeb8fb71b783b
balping <balping314@gmail.com>
2016-01-16 03:33:27 +0100

    line wrapping in codemirror; html formatting (beutifying) still needed

--- src/Views/tickets/partials/summernote.blade.php
+++ src/Views/tickets/partials/summernote.blade.php
@@ -18,7 +18,7 @@
 
     $(function() {
 
-        var options = $.extend(true, {lang: '{{$editor_locale}}' {!! $codemirror_enabled ? ", codemirror: {theme: '{$codemirror_theme}', mode: 'text/html', htmlMode: true}" : ''  !!} } , {!! $editor_options !!});
+        var options = $.extend(true, {lang: '{{$editor_locale}}' {!! $codemirror_enabled ? ", codemirror: {theme: '{$codemirror_theme}', mode: 'text/html', htmlMode: true, lineWrapping: true}" : ''  !!} } , {!! $editor_options !!});
 
         $("textarea.summernote-editor").summernote(options);
 

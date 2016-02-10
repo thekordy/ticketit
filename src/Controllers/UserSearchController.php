7097f74757a68dc0eaf9d78c638519ad8cd3e246
tmcdon89 <tmcdonald@Toms-MacBook-Pro.local>
2016-02-10 12:46:41 -0600

    Use Request instead of input comment out examples

--- src/Controllers/UserSearchController.php
+++ src/Controllers/UserSearchController.php
@@ -3,14 +3,14 @@
 namespace Kordy\Ticketit\Controllers;
 
 use App\Http\Controllers\Controller;
-use Input;
+use Illuminate\Http\Request;
 use App\User;
 
 class UserSearchController extends Controller {
 
-    public function userSearch() {
+    public function userSearch(Request $request) {
 
-        $keyword = Input::get('q'); 
+        $keyword = $request->get('q'); 
         // If you wanted to search multiple Terms you could do something like the following
         //$searchRaw = Input::get('q');
         //$keywords = explode(" ", $searchRaw);
@@ -20,11 +20,11 @@ class UserSearchController extends Controller {
         // This is where you put additional fields to be searched through
         $user_list->orWhere(function ($query) use ($keyword) {
             $query->orWhere('users.email', 'LIKE', '%' . $keyword . '%');
-            $query->orWhere('users.last_name', 'LIKE', '%' . $keyword . '%');
-            $query->orWhere('users.first_name', 'LIKE', '%' . $keyword . '%');
-            $query->orWhere('users.business_name', 'LIKE', '%' . $keyword . '%');
-            $query->orWhere('users.phone_1', 'LIKE', '%' . $keyword . '%');
-            $query->orWhere('users.phone_2', 'LIKE', '%' . $keyword . '%');
+//            $query->orWhere('users.last_name', 'LIKE', '%' . $keyword . '%');
+//            $query->orWhere('users.first_name', 'LIKE', '%' . $keyword . '%');
+//            $query->orWhere('users.business_name', 'LIKE', '%' . $keyword . '%');
+//            $query->orWhere('users.phone_1', 'LIKE', '%' . $keyword . '%');
+//            $query->orWhere('users.phone_2', 'LIKE', '%' . $keyword . '%');
         });
         
         $data = $user_list->get();

884d5dbb8691ac428bca5ca73c14cc1ca11ec044
Ahmed Kordy <ahmed@kordy.info>
2016-02-11 02:32:04 -0500

    Applied fixes from StyleCI

--- src/Controllers/UserSearchController.php
+++ src/Controllers/UserSearchController.php
@@ -3,32 +3,31 @@
 namespace Kordy\Ticketit\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Http\Request;
 
-class UserSearchController extends Controller {
-
-    public function userSearch(Request $request) {
-
-        $keyword = $request->get('q'); 
+class UserSearchController extends Controller
+{
+    public function userSearch(Request $request)
+    {
+        $keyword = $request->get('q');
         // If you wanted to search multiple Terms you could do something like the following
         //$searchRaw = Input::get('q');
         //$keywords = explode(" ", $searchRaw);
 
-        $user_list = User::Where('users.name', 'LIKE', '%' . $keyword . '%');
-        
+        $user_list = User::Where('users.name', 'LIKE', '%'.$keyword.'%');
+
         // This is where you put additional fields to be searched through
         $user_list->orWhere(function ($query) use ($keyword) {
-            $query->orWhere('users.email', 'LIKE', '%' . $keyword . '%');
+            $query->orWhere('users.email', 'LIKE', '%'.$keyword.'%');
 //            $query->orWhere('users.last_name', 'LIKE', '%' . $keyword . '%');
 //            $query->orWhere('users.first_name', 'LIKE', '%' . $keyword . '%');
 //            $query->orWhere('users.business_name', 'LIKE', '%' . $keyword . '%');
 //            $query->orWhere('users.phone_1', 'LIKE', '%' . $keyword . '%');
 //            $query->orWhere('users.phone_2', 'LIKE', '%' . $keyword . '%');
         });
-        
+
         $data = $user_list->get();
         echo json_encode($data);
     }
-
 }

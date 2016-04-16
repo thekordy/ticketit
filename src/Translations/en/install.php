1f35387abaeca15087bde4a632e7a59ebd31a995
balping <balping314@gmail.com>
2016-04-16 23:15:33 +0200

    Update install.php

--- src/Translations/en/install.php
+++ src/Translations/en/install.php
@@ -5,7 +5,7 @@ return [
     'initial-setup'                   => 'Ticketit Initial Setup',
     'master-template-file'            => 'Master template file',
     'master-template-other-path'      => 'Other path to the master template file',
-    'master-template-other-path-ex'   => 'ex. views/layout/app.blade.php',
+    'master-template-other-path-ex'   => 'ex. views/layouts/app.blade.php',
     'migrations-to-be-installed'      => 'These migrations will be installed:',
     'all-tables-migrated'             => 'All needed tables are migrated',
     'proceed'                         => 'Proceed',

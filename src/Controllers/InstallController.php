445cc35040f62e7542b8e54895347d7c660cf962
Rob Stiles <robs@codexsoftware.co.uk>
2016-04-30 12:17:27 +0100

    Revert "Fixes initial migration error during clean installation"
    TicketitServiceProvider would not boot properly as all migrations were showing as having
    not run.
    
    This reverts commit f21a7fd0fbb4e8e8c0d8c7b776364720bc498b80.

--- src/Controllers/InstallController.php
+++ src/Controllers/InstallController.php
@@ -214,7 +214,6 @@ class InstallController extends Controller
             $migration_arr [] = $migration_parent->migration;
         }
 
-        $migration_arr = array();
         foreach ($tables as $table) {
             if (!in_array($table, $migration_arr)) {
                 $inactiveMigrations [] = $table;

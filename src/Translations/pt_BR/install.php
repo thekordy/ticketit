3c3699e3788001fc9b94783117eb2b17503cd499
Balázs Kovács <balping314@gmail.com>
2016-05-29 16:09:51 +0200

    Installation: example blade path corrected

--- src/Translations/pt_BR/install.php
+++ src/Translations/pt_BR/install.php
@@ -4,7 +4,7 @@ return [
     'initial-setup'                   => 'Configuração Inicial dos Chamados',
     'master-template-file'            => 'Arquivo de Modelo Principal',
     'master-template-other-path'      => 'Outro Caminho para o Arquivo de Modelo Principal',
-    'master-template-other-path-ex'   => 'ex. views/layout/app.blade.php',
+    'master-template-other-path-ex'   => 'ex. views/layouts/app.blade.php',
     'migrations-to-be-installed'      => 'Esta Migração será Instalada:',
     'all-tables-migrated'             => 'Todas as Tabelas serão Migradas',
     'proceed'                         => 'Continuar',

01716c4c8dd5567780c9748f316f78d826fa9783
Ahmed Kordy <ahmed@kordy.info>
2016-08-19 22:17:32 +0000

    Applied fixes from StyleCI

--- src/Helpers/TicketitHelpers.php
+++ src/Helpers/TicketitHelpers.php
@@ -2,27 +2,28 @@
 
 namespace Kordy\Ticketit\Helpers;
 
-
 class TicketitHelpers
 {
-
     /**
-     * Get route name from Ticketit routes configuration
+     * Get route name from Ticketit routes configuration.
      *
      * @param string $route_config_name
+     *
      * @return string
      */
     public function getApiRouteName($route_config_name)
     {
         $routes = config('ticketit.routes');
+
         return $routes['api'][$route_config_name]['parameters']['as'];
     }
 
     /**
-     * Get route full path from Ticketit routes configuration
+     * Get route full path from Ticketit routes configuration.
      *
      * @param string $route_config_name
-     * @param mixed $param (optional)
+     * @param mixed  $param             (optional)
+     *
      * @return string
      */
     public function getApiRoutePath($route_config_name, $param = null)
@@ -31,6 +32,7 @@ class TicketitHelpers
         if ($param) {
             return route($route_name, $param);
         }
+
         return route($route_name);
     }
 }

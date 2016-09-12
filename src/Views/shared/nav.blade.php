90ac75c6777648a30d680dde9b4dec08170ecd06
balping <balping314@gmail.com>
2016-09-12 14:56:33 +0200

    Fix #250

--- src/Views/shared/nav.blade.php
+++ src/Views/shared/nav.blade.php
@@ -4,14 +4,30 @@
             <li role="presentation" class="{!! $tools->fullUrlIs(action('\Kordy\Ticketit\Controllers\TicketsController@index')) ? "active" : "" !!}">
                 <a href="{{ action('\Kordy\Ticketit\Controllers\TicketsController@index') }}">{{ trans('ticketit::lang.nav-active-tickets') }}
                     <span class="badge">
-                        {{ Kordy\Ticketit\Models\Ticket::active()->agentUserTickets($u->id)->count() }}
+                         <?php 
+                            if ($u->isAdmin()) {
+                                echo Kordy\Ticketit\Models\Ticket::active()->count();
+                            } elseif ($u->isAgent()) {
+                                echo Kordy\Ticketit\Models\Ticket::active()->agentUserTickets($u->id)->count();
+                            } else {
+                                echo Kordy\Ticketit\Models\Ticket::userTickets($u->id)->active()->count();
+                            }
+                        ?>
                     </span>
                 </a>
             </li>
             <li role="presentation" class="{!! $tools->fullUrlIs(action('\Kordy\Ticketit\Controllers\TicketsController@indexComplete')) ? "active" : "" !!}">
                 <a href="{{ action('\Kordy\Ticketit\Controllers\TicketsController@indexComplete') }}">{{ trans('ticketit::lang.nav-completed-tickets') }}
                     <span class="badge">
-                        {{ Kordy\Ticketit\Models\Ticket::complete()->agentUserTickets($u->id)->count() }}
+                        <?php 
+                            if ($u->isAdmin()) {
+                                echo Kordy\Ticketit\Models\Ticket::complete()->count();
+                            } elseif ($u->isAgent()) {
+                                echo Kordy\Ticketit\Models\Ticket::complete()->agentUserTickets($u->id)->count();
+                            } else {
+                                echo Kordy\Ticketit\Models\Ticket::userTickets($u->id)->complete()->count();
+                            }
+                        ?>
                     </span>
                 </a>
             </li>

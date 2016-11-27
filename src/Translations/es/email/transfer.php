8fcd3277e7b1e2e31545105d40427665f4e4ce19
mandrakecr <brauliomendez@gmail.com>
2016-11-27 16:36:35 -0600

    Small fix to test on translation

--- src/Translations/es/email/transfer.php
+++ src/Translations/es/email/transfer.php
@@ -2,6 +2,6 @@
 
 return [
 
-'data' => '<b>:name</b> transfirió el tiquete "<b>:subject</b>" d :agent en :old_category a usted en :new_category<br>',
+'data' => '<b>:name</b> transfirió el tiquete "<b>:subject</b>" de :agent en :old_category a usted en :new_category<br>',
 
 ];

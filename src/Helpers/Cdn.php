5dadff774ac54d9a3b90f0adee0d9146d029fa01
balping <balping314@gmail.com>
2017-07-16 21:23:54 +0200

    Update Cdn.php

--- src/Helpers/Cdn.php
+++ src/Helpers/Cdn.php
@@ -10,7 +10,7 @@ namespace Kordy\Ticketit\Helpers;
  */
 class Cdn
 {
-    const CodeMirror = '5.26.0';
-    const Summernote = '0.8.3';
+    const CodeMirror = '5.27.4';
+    const Summernote = '0.8.6';
     const FontAwesome = '4.7.0';
 }

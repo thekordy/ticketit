89be457bd9dcbb31ac1b5cbc560b18b82d15df2e
imanghafoori <imanghafoori1@gmail.com>
2017-09-10 17:46:15 +0430

    Widgetize Priority Index.

--- src/Views/admin/priority/index.blade.php
+++ src/Views/admin/priority/index.blade.php
@@ -7,69 +7,7 @@
 @section('content')
     @include('ticketit::shared.header')
     <div class="panel panel-default">
-        <div class="panel-heading">
-            <h2>{{ trans('ticketit::admin.priority-index-title') }}
-                {!! link_to_route(
-                                    $setting->grab('admin_route').'.priority.create',
-                                    trans('ticketit::admin.btn-create-new-priority'), null,
-                                    ['class' => 'btn btn-primary pull-right'])
-                !!}
-            </h2>
-        </div>
-
-        @if ($priorities->isEmpty())
-            <h3 class="text-center">{{ trans('ticketit::admin.priority-index-no-priorities') }}
-                {!! link_to_route($setting->grab('admin_route').'.priority.create', trans('ticketit::admin.priority-index-create-new')) !!}
-            </h3>
-        @else
-            <div id="message"></div>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <td>{{ trans('ticketit::admin.table-id') }}</td>
-                        <td>{{ trans('ticketit::admin.table-name') }}</td>
-                        <td>{{ trans('ticketit::admin.table-action') }}</td>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($priorities as $priority)
-                    <tr>
-                        <td style="vertical-align: middle">
-                            {{ $priority->id }}
-                        </td>
-                        <td style="color: {{ $priority->color }}; vertical-align: middle">
-                            {{ $priority->name }}
-                        </td>
-                        <td>
-                            {!! link_to_route(
-                                                    $setting->grab('admin_route').'.priority.edit', trans('ticketit::admin.btn-edit'), $priority->id,
-                                                    ['class' => 'btn btn-info'] )
-                                !!}
-
-                                {!! link_to_route(
-                                                    $setting->grab('admin_route').'.priority.destroy', trans('ticketit::admin.btn-delete'), $priority->id,
-                                                    [
-                                                    'class' => 'btn btn-danger deleteit',
-                                                    'form' => "delete-$priority->id",
-                                                    "node" => $priority->name
-                                                    ])
-                                !!}
-                            {!! CollectiveForm::open([
-                                            'method' => 'DELETE',
-                                            'route' => [
-                                                        $setting->grab('admin_route').'.priority.destroy',
-                                                        $priority->id
-                                                        ],
-                                            'id' => "delete-$priority->id"
-                                            ])
-                            !!}
-                            {!! CollectiveForm::close() !!}
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        @endif
+        @widget('\Kordy\Ticketit\Widgets\PriorityIndex', ['admin_route' => $setting->grab('admin_route')])
     </div>
 @stop
 @section('footer')
---
+++ src/Widgetize/PriorityIndex.php
@@ -0,0 +1,18 @@
+<?php
+namespace Kordy\Ticketit\Widgetize;
+
+use Kordy\Ticketit\Models\Priority;
+
+class PriorityIndex
+{
+    public $template = 'ticketit::admin.priority.widgets.index_table';
+    public $cacheLifeTime = 60;
+    public $cacheTags = ['priority'];
+
+    public function data($admin_route)
+    {
+        return ['admin_route' => $admin_route, 'priorities' => Priority::all()];
+    }
+
+
+}

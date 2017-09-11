52b82fe2e7c27e9eed863fab3ea3bfb105d8cdc8
imanghafoori <imanghafoori1@gmail.com>
2017-09-11 14:00:14 +0430

    DRY up view files

--- src/Views/admin/configuration/datails_table.blade.php
+++ src/Views/admin/configuration/datails_table.blade.php
@@ -9,16 +9,18 @@
         <th class="text-center">{{ trans('ticketit::admin.table-edit') }}</th>
         </thead>
         <tbody>
-        @foreach($configurations_by_sections['other'] as $configuration)
+        @foreach($configs as $configuration)
             <tr>
                 <td class="text-center">{!! $configuration->id !!}</td>
                 <td>{!! $configuration->slug !!}</td>
                 <td>{!! $configuration->default !!}</td>
-                <td><a href="{!! route($setting->grab('admin_route').'.configuration.edit', [$configuration->id]) !!}" title="{{ trans('ticketit::admin.table-edit').' '.$configuration->slug }}" data-toggle="tooltip">{!! $configuration->value !!}</a></td>
+                <td><a href="{!! route($admin_route.'.configuration.edit', [$configuration->id]) !!}"
+                       title="{{ trans('ticketit::admin.table-edit').' '.$configuration->slug }}"
+                       data-toggle="tooltip">{!! $configuration->value !!}</a></td>
                 <td class="text-center">{!! $configuration->lang !!}</td>
                 <td class="text-center">
                     {!! link_to_route(
-                        $setting->grab('admin_route').'.configuration.edit', trans('ticketit::admin.btn-edit'), [$configuration->id],
+                        $admin_route.'.configuration.edit', trans('ticketit::admin.btn-edit'), [$configuration->id],
                         ['class' => 'btn btn-info', 'title' => trans('ticketit::admin.table-edit').' '.$configuration->slug,  'data-toggle' => 'tooltip'] )
                     !!}
                 </td>

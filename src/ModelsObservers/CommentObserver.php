4b7ec276baaa5d7b3256671fd8fb90a646da25e7
imanghafoori <imanghafoori1@gmail.com>
2018-08-28 00:02:53 +0430

    put the code in the observers

--- src/ModelsObservers/CommentObserver.php
+++ src/ModelsObservers/CommentObserver.php
@@ -2,6 +2,20 @@
 
 namespace Kordy\Ticketit\ModelsObservers;
 
+use Kordy\Ticketit\Controllers\NotificationsController;
+use Kordy\Ticketit\Models\Comment;
+use Kordy\Ticketit\Models\Setting;
+
 class CommentObserver
 {
+    public function creating()
+    {
+        Comment::creating(function ($comment) {
+            if (Setting::grab('comment_notification')) {
+                $notification = new NotificationsController();
+                $notification->newComment($comment);
+            }
+        });
+
+    }
 }
--- src/ModelsObservers/TicketObserver.php
+++ src/ModelsObservers/TicketObserver.php
@@ -2,6 +2,44 @@
 
 namespace Kordy\Ticketit\ModelsObservers;
 
+use Kordy\Ticketit\Controllers\NotificationsController;
+use Kordy\Ticketit\Models\Setting;
+use Kordy\Ticketit\Models\Ticket;
+
 class TicketObserver
 {
+    public function created()
+    {
+        // Send notification when ticket status is modified
+        Ticket::created(function ($ticket) {
+            if (Setting::grab('assigned_notification')) {
+                $notification = new NotificationsController();
+                $notification->newTicketNotifyAgent($ticket);
+            }
+
+            return true;
+        });
+    }
+
+    public function updating()
+    {
+        Ticket::updating(function ($modified_ticket) {
+            if (Setting::grab('status_notification')) {
+                $original_ticket = Ticket::find($modified_ticket->id);
+                if ($original_ticket->status_id != $modified_ticket->status_id || $original_ticket->completed_at != $modified_ticket->completed_at) {
+                    $notification = new NotificationsController();
+                    $notification->ticketStatusUpdated($modified_ticket, $original_ticket);
+                }
+            }
+            if (Setting::grab('assigned_notification')) {
+                $original_ticket = Ticket::find($modified_ticket->id);
+                if ($original_ticket->agent->id != $modified_ticket->agent->id) {
+                    $notification = new NotificationsController();
+                    $notification->ticketAgentUpdated($modified_ticket, $original_ticket);
+                }
+            }
+
+            return true;
+        });
+    }
 }

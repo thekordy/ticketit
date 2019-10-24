0559410aada9b0828920944fe8ef71294ff32d3f
Rick Bakker <rickbakkr@gmail.com>
2019-10-24 17:11:50 +0200

    Dutch translation

---
+++ src/Translations/nl/email/assigned.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+'data' => '
+	<b>:name</b> heeft een ticket aangemaakt, getiteld <b>:subject</b><br>
+	:status in :category, en is aan jou toegewezen.<br>
+',
+
+];
---
+++ src/Translations/nl/email/transfer.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+'data' => '<b>:name</b> heeft ticket "<b>:subject</b>" verplaatst van :agent in :old_category naar jou in :new_category<br>',
+
+];

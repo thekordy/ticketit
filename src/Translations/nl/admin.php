2a9d6ff77b104376dabc56ace9f4d987deba3f53
Rick Bakker <rickbakkr@gmail.com>
2019-10-24 22:44:46 +0200

    Added admin

--- src/Translations/nl/admin.php
+++ src/Translations/nl/admin.php
@@ -5,38 +5,38 @@ return [
  /*
   *  Constants
   */
-  'nav-settings'                  => 'Settings',
-  'nav-agents'                    => 'Agents',
+  'nav-settings'                  => 'Instellingen',
+  'nav-agents'                    => 'Medewerkers',
   'nav-dashboard'                 => 'Dashboard',
-  'nav-categories'                => 'Categories',
-  'nav-priorities'                => 'Priorities',
-  'nav-statuses'                  => 'Statuses',
-  'nav-configuration'             => 'Configuration',
-  'nav-administrator'             => 'Administrator',  //new
+  'nav-categories'                => 'Categorieën',
+  'nav-priorities'                => 'Prioriteiten',
+  'nav-statuses'                  => 'Statussen',
+  'nav-configuration'             => 'Configuratie',
+  'nav-administrator'             => 'Beheerder',  //new
 
   'table-hash'                    => '#',
   'table-id'                      => 'ID',
-  'table-name'                    => 'Name',
-  'table-action'                  => 'Action',
-  'table-categories'              => 'Categories',
-  'table-join-category'           => 'Joined Categories',
-  'table-remove-agent'            => 'Remove from agents',
-  'table-remove-administrator'    => 'Remove from administrators', // New
+  'table-name'                    => 'Onderwerp',
+  'table-action'                  => 'Actie',
+  'table-categories'              => 'Categorie',
+  'table-join-category'           => 'Categorieën',
+  'table-remove-agent'            => 'Verwijderen van medewerkers',
+  'table-remove-administrator'    => 'Verwijderen van beheerders', // New
 
   'table-slug'                    => 'Slug',
-  'table-default'                 => 'Default Value',
-  'table-value'                   => 'My Value',
+  'table-default'                 => 'Standaardwaarde',
+  'table-value'                   => 'Mijn waarde',
   'table-lang'                    => 'Lang',
-  'table-edit'                    => 'Edit',
+  'table-edit'                    => 'Wijzigen',
 
-  'btn-back'                      => 'Back',
-  'btn-delete'                    => 'Delete',
-  'btn-edit'                      => 'Edit',
-  'btn-join'                      => 'Join',
-  'btn-remove'                    => 'Remove',
-  'btn-submit'                    => 'Submit',
-  'btn-save'                      => 'Save',
-  'btn-update'                    => 'Update',
+  'btn-back'                      => 'Terug',
+  'btn-delete'                    => 'Verwijderen',
+  'btn-edit'                      => 'Bewerken',
+  'btn-join'                      => 'Toevoegen',
+  'btn-remove'                    => 'Verwijderen',
+  'btn-submit'                    => 'Opsturen',
+  'btn-save'                      => 'Opslaan',
+  'btn-update'                    => 'Updaten',
 
   'colon'                         => ': ',
 
@@ -46,116 +46,116 @@ return [
 
 // tickets-admin/____
   'index-title'                         => 'Tickets System Dashboard',
-  'index-empty-records'                 => 'No tickets yet',
-  'index-total-tickets'                 => 'Total tickets',
+  'index-empty-records'                 => 'Nog geen tickets',
+  'index-total-tickets'                 => 'Totaal aantal tickets',
   'index-open-tickets'                  => 'Open tickets',
-  'index-closed-tickets'                => 'Closed tickets',
+  'index-closed-tickets'                => 'Gesloten tickets',
   'index-performance-indicator'         => 'Performance Indicator',
-  'index-periods'                       => 'Periods',
-  'index-3-months'                      => '3 months',
-  'index-6-months'                      => '6 months',
-  'index-12-months'                     => '12 months',
-  'index-tickets-share-per-category'    => 'Tickets share per category',
-  'index-tickets-share-per-agent'       => 'Tickets share per agent',
-  'index-categories'                    => 'Categories',
-  'index-category'                      => 'Category',
-  'index-agents'                        => 'Agents',
-  'index-agent'                         => 'Agent',
-  'index-administrators'                => 'Administrators',  //new
-  'index-administrator'                 => 'Administrator',  //new
-  'index-users'                         => 'Users',
-  'index-user'                          => 'User',
+  'index-periods'                       => 'Periodes',
+  'index-3-months'                      => '3 maand',
+  'index-6-months'                      => '6 maand',
+  'index-12-months'                     => '12 maand',
+  'index-tickets-share-per-category'    => 'Aandeel per categorie',
+  'index-tickets-share-per-agent'       => 'Aandeel per medewerker',
+  'index-categories'                    => 'Categorieën',
+  'index-category'                      => 'Categorie',
+  'index-agents'                        => 'Medewerkers',
+  'index-agent'                         => 'Medewerker',
+  'index-administrators'                => 'Beheerders',  //new
+  'index-administrator'                 => 'Beheerder',  //new
+  'index-users'                         => 'Gebruikers',
+  'index-user'                          => 'Gebruiker',
   'index-tickets'                       => 'Tickets',
-  'index-open'                          => 'Open',
-  'index-closed'                        => 'Closed',
-  'index-total'                         => 'Total',
-  'index-month'                         => 'Month',
-  'index-performance-chart'             => 'How many days in average to resolve a ticket?',
-  'index-categories-chart'              => 'Tickets distribution per category',
-  'index-agents-chart'                  => 'Tickets distribution per Agent',
+  'index-open'                          => 'Lopend',
+  'index-closed'                        => 'Afgerond',
+  'index-total'                         => 'Totaal',
+  'index-month'                         => 'Maand',
+  'index-performance-chart'             => 'Gemiddeld aantal dagen voor oplossen ticket',
+  'index-categories-chart'              => 'Ticket verdeling per categorie',
+  'index-agents-chart'                  => 'Ticket verdeling per medewerker',
 
 // tickets-admin/agent/____
   'agent-index-title'             => 'Agent Management',
-  'btn-create-new-agent'          => 'Create new agent',
-  'agent-index-no-agents'         => 'There are no agents, ',
-  'agent-index-create-new'        => 'Add agents',
-  'agent-create-title'            => 'Add Agent',
-  'agent-create-add-agents'       => 'Add Agents',
-  'agent-create-no-users'         => 'There are no user accounts, create user accounts first.',
-  'agent-create-select-user'      => 'Select user accounts to be added as agents',
+  'btn-create-new-agent'          => 'Medewerker toevoegen',
+  'agent-index-no-agents'         => 'Er zijn geen medewerkers, ',
+  'agent-index-create-new'        => 'Nieuwe medewerkers',
+  'agent-create-title'            => 'Nieuwe medewerker',
+  'agent-create-add-agents'       => 'Nieuwe medewerkers',
+  'agent-create-no-users'         => 'Nog geen user accounts, maak deze eerst.',
+  'agent-create-select-user'      => 'Selecteer user accounts, waarvan je medewerkers wilt maken',
 
 // tickets-admin/administrators/____
-  'administrator-index-title'                   => 'Administrator Management',  //new
-  'btn-create-new-administrator'                => 'Create new administrator',  //new
-  'administrator-index-no-administrators'       => 'There are no administrators, ',  //new
-  'administrator-index-create-new'              => 'Add administrators',  //new
-  'administrator-create-title'                  => 'Add Administrator',  //new
-  'administrator-create-add-administrators'     => 'Add Administrators',  //new
-  'administrator-create-no-users'               => 'There are no user accounts, create user accounts first.',  //new
-  'administrator-create-select-user'            => 'Select user accounts to be added as administrators',  //new
+  'administrator-index-title'                   => 'Beheerders beheer',  //new
+  'btn-create-new-administrator'                => 'Nieuwe beheerder aanmaken',  //new
+  'administrator-index-no-administrators'       => 'Er zijn geen beheerders, ',  //new
+  'administrator-index-create-new'              => 'Beheerder aanmaken',  //new
+  'administrator-create-title'                  => 'Beheerder aanmaken',  //new
+  'administrator-create-add-administrators'     => 'Beheerders aanmaken',  //new
+  'administrator-create-no-users'               => 'Nog geen user accounts, maak deze eerst.',  //new
+  'administrator-create-select-user'            => 'Selecteer user accounts, waarvan je beheerders wilt maken',  //new
 
 // tickets-admin/category/____
-  'category-index-title'          => 'Categories Management',
-  'btn-create-new-category'       => 'Create new category',
-  'category-index-no-categories'  => 'There are no categories, ',
-  'category-index-create-new'     => 'create new category',
-  'category-index-js-delete'      => 'Are you sure you want to delete the category: ',
-  'category-create-title'         => 'Create New Category',
-  'category-create-name'          => 'Name',
-  'category-create-color'         => 'Color',
-  'category-edit-title'           => 'Edit Category: :name',
+  'category-index-title'          => 'Categoriebeheer',
+  'btn-create-new-category'       => 'Categorie aanmaken',
+  'category-index-no-categories'  => 'Er zijn geen categorieën, ',
+  'category-index-create-new'     => 'categorie aanmaken',
+  'category-index-js-delete'      => 'Wil je deze categorie zeker verwijderen: ',
+  'category-create-title'         => 'Nieuwe categorie aanmaken',
+  'category-create-name'          => 'Naam',
+  'category-create-color'         => 'Kleur',
+  'category-edit-title'           => 'Categorie bewerken: :name',
 
 // tickets-admin/priority/____
-  'priority-index-title'          => 'Priorities Management',
-  'btn-create-new-priority'       => 'Create new priority',
-  'priority-index-no-priorities'  => 'There are no priorities, ',
-  'priority-index-create-new'     => 'create new priority',
-  'priority-index-js-delete'      => 'Are you sure you want to delete the priority: ',
-  'priority-create-title'         => 'Create New Priority',
-  'priority-create-name'          => 'Name',
-  'priority-create-color'         => 'Color',
-  'priority-edit-title'           => 'Edit Priority: :name',
+  'priority-index-title'          => 'Prioriteitenbeheer',
+  'btn-create-new-priority'       => 'Nieuwe prioriteit aanmaken',
+  'priority-index-no-priorities'  => 'Er zijn geen prioriteiten, ',
+  'priority-index-create-new'     => 'Nieuwe prioriteit aanmaken',
+  'priority-index-js-delete'      => 'Weet je zeker dat je deze prioriteit wilt verwijderen: ',
+  'priority-create-title'         => 'Nieuwe prioriteit aanmaken',
+  'priority-create-name'          => 'Naam',
+  'priority-create-color'         => 'Kleur',
+  'priority-edit-title'           => 'Prioriteit bewerken: :name',
 
 // tickets-admin/status/____
-  'status-index-title'            => 'Statuses Management',
-  'btn-create-new-status'         => 'Create new status',
-  'status-index-no-statuses'      => 'There are no statues,',
-  'status-index-create-new'       => 'create new status',
-  'status-index-js-delete'        => 'Are you sure you want to delete the status: ',
-  'status-create-title'           => 'Create New Status',
-  'status-create-name'            => 'Name',
-  'status-create-color'           => 'Color',
-  'status-edit-title'             => 'Edit Status: :name',
+  'status-index-title'            => 'Statusbeheer',
+  'btn-create-new-status'         => 'Nieuwe status aanmaken',
+  'status-index-no-statuses'      => 'Er zijn geen statussen,',
+  'status-index-create-new'       => 'Nieuwe status aanmaken',
+  'status-index-js-delete'        => 'Weet je zeker dat je deze status wilt verwijderen: ',
+  'status-create-title'           => 'Nieuwe status aanmaken',
+  'status-create-name'            => 'Naam',
+  'status-create-color'           => 'Kleur',
+  'status-edit-title'             => 'Status bewerken: :name',
 
 // tickets-admin/configuration/____
-  'config-index-title'            => 'Configuration Settings',
-  'config-index-subtitle'         => 'Settings',
-  'btn-create-new-config'         => 'Add new setting',
-  'config-index-no-settings'      => 'There are no settings,',
+  'config-index-title'            => 'Instellingen',
+  'config-index-subtitle'         => 'instellingen',
+  'btn-create-new-config'         => 'Nieuwe instelling toevoegen',
+  'config-index-no-settings'      => 'Er zijn geen instellingen,',
   'config-index-initial'          => 'Initial',
   'config-index-tickets'          => 'Tickets',
-  'config-index-notifications'    => 'Notifications',
-  'config-index-permissions'      => 'Permissions',
+  'config-index-notifications'    => 'Notificaties',
+  'config-index-permissions'      => 'Rechten',
   'config-index-editor'           => 'Editor', //Added: 2016.01.14
-  'config-index-other'            => 'Other',
-  'config-create-title'           => 'Create: New Global Setting',
-  'config-create-subtitle'        => 'Create Setting',
-  'config-edit-title'             => 'Edit: Global Configuration',
-  'config-edit-subtitle'          => 'Edit Setting',
+  'config-index-other'            => 'Overige',
+  'config-create-title'           => 'Nieuwe globale instelling',
+  'config-create-subtitle'        => 'Instelling aanmaken',
+  'config-edit-title'             => 'Globale instelling bewerken',
+  'config-edit-subtitle'          => 'Bewerk instelling',
   'config-edit-id'                => 'ID',
   'config-edit-slug'              => 'Slug',
-  'config-edit-default'           => 'Default value',
-  'config-edit-value'             => 'My value',
-  'config-edit-language'          => 'Language',
+  'config-edit-default'           => 'Standaardwaarde',
+  'config-edit-value'             => 'Mijn waarde',
+  'config-edit-language'          => 'Taal',
   'config-edit-unserialize'       => 'Get the array values, and change the values',
   'config-edit-serialize'         => 'Get the serialized string of the changed values (to be entered in the field)',
   'config-edit-should-serialize'  => 'Serialize', //Added: 2016-01-16
   'config-edit-eval-warning'      => 'When checked, the server will run eval()!
   									  Don\'t use this if eval() is disabled on your server or if you don\'t exactly know what you are doing!
   									  Exact code executed:', //Added: 2016-01-16
-  'config-edit-reenter-password'  => 'Re-enter your password', //Added: 2016-01-16
-  'config-edit-auth-failed'       => 'Password mismatch', //Added: 2016-01-16
-  'config-edit-eval-error'        => 'Invalid value', //Added: 2016-01-16
+  'config-edit-reenter-password'  => 'Voer jouw wachtwoord nog eens in', //Added: 2016-01-16
+  'config-edit-auth-failed'       => 'Wachtwoorden komt niet overeen', //Added: 2016-01-16
+  'config-edit-eval-error'        => 'Ongeldige waarde', //Added: 2016-01-16
   'config-edit-tools'             => 'Tools:',
 
 ];
